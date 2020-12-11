ab2e07c347522106759381faa3248efe2b10a2e3
ZePotente <ezeposada@gmail.com>
2020-12-11 16:58:13 -0300

    Funcion de prueba.

--- ENL_FUNC.f90
+++ ENL_FUNC.f90
@@ -5,13 +5,13 @@ CONTAINS
         REAL(8) :: F
         REAL(8), INTENT(IN) :: X
         
-        F = X
+        F = X*X*X + 1
     END FUNCTION
     
     FUNCTION DF(X)
         REAL(8) :: DF
         REAL(8), INTENT(IN) :: X
         
-        DF = X
+        DF = 3*X*X
     END FUNCTION
 END MODULE
