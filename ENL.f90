692d9a970568696783ba203a96db0e3726495df8
ZePotente <ezeposada@gmail.com>
2020-12-11 17:17:41 -0300

    Programa terminado.

--- ENL.f90
+++ ENL.f90
@@ -1,6 +1,7 @@
 PROGRAM P
     !Modulo
     USE ENL_MET
+    USE ENL_FUNC
     IMPLICIT NONE
     
     REAL(8) :: A, B, XINI, XRES, XRES2
@@ -19,7 +20,37 @@ PROGRAM P
     PRINT *, 'Empezando resolución por Punto Fijo Sistemático'
     CALL ENL_PUNTOFIJO_SIST(A, B, TOL, XRES2, XINI)
     PRINT *, 'El valor de la raíz es: ', XRES2
+    
+    CALL GUARDAR_FUNCION(A-5, B+5)
+    CALL GUARDAR_RAICES(XRES)
+    CALL SYSTEM("gnuplot script_func.p")
 CONTAINS
     
+    !Guarda los valores de X y F(X) (funcion en ENL_FUNC), para graficarlos.
+    !Supongo A < B
+    SUBROUTINE GUARDAR_FUNCION(A, B)
+        REAL(8), INTENT(IN) :: A, B
+        !
+        REAL(8) :: H, XACT
+        H = 1D-3; XACT = A
+        
+        OPEN(1, FILE = 'Funcion.txt', ACTION = 'WRITE')
     
+        DO WHILE(XACT < B)
+            WRITE(1, '(2F15.8)') XACT, F(XACT)
+            XACT = XACT + H
+        END DO
+        WRITE(1, '(2F15.8)') XACT, F(XACT)
+        CLOSE(1)
+    END SUBROUTINE
+    
+    !Se supone que guarde un vector de raíces, pero las estoy usando por separado.
+    !Así que X es una simple raíz
+    SUBROUTINE GUARDAR_RAICES(X)
+        REAL(8), INTENT(IN) :: X
+        !
+        OPEN(1, FILE = 'Raices.txt', ACTION = 'WRITE')
+        WRITE(1, '(2F15.8)') X, F(X)
+        CLOSE(1)
+    END SUBROUTINE
 END PROGRAM
